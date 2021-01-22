d9172f559a24777c45b64aefdbe6048887493adf
haydenanderl33 <hayden.anderl33@gmail.com>
2021-01-22 13:45:01 -0700

    added info section, need to fix underline scrool on header

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,48 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: '/images/svg-1.svg',
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+export const homeObjTwo = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: '/images/svg-1.svg',
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+export const homeObjThree = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: '/images/svg-1.svg',
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
