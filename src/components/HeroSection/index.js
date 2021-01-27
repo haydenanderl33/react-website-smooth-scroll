19a4c9dfcd7cae828be83a596db1760effd3e4ef
haydenanderl33 <hayden.anderl33@gmail.com>
2021-01-27 14:27:53 -0700

    finished

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,32 +1,54 @@
-import React, {useState} from 'react'
-import Video from '../../video/video.mp4'
-import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements'
-import { Button } from '../ButtonElements'
+import React, { useState } from "react";
+import Video from "../../video/video.mp4";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import { Button } from "../ButtonElements";
 const HeroSection = () => {
-    const [hover, setHover] = useState(false)
+  const [hover, setHover] = useState(false);
 
-    const onHover = () => {
-        setHover(!hover)
-    }
+  const onHover = () => {
+    setHover(!hover);
+  };
 
-    return (
-        <HeroContainer>
-            <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
-            </HeroBg>
-            <HeroContent>
-                <HeroH1>Virtual Banking Made Easy</HeroH1>
-                <HeroP>
-                    Sign up for a new account today and receive $250 in credit towards your next payment
-                </HeroP>
-                <HeroBtnWrapper>
-                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true" >
-                        Get started {hover ? <ArrowForward/> : <ArrowRight/> }
-                    </Button>
-                </HeroBtnWrapper>
-            </HeroContent>
-        </HeroContainer>
-    )
-}
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards
+          your next payment
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact={"true"}
+            offset={-80}
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
